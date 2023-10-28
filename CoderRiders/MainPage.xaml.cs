f42dbe92a943c2c86edf76231ce28c79378f723f
Leander <67602400+LeanderM99@users.noreply.github.com>
2023-10-28 12:40:07 +0200

    Init

---
+++ CoderRiders/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CoderRiders
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CoderRiders/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CoderRiders
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
