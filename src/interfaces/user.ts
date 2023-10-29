4709e08a94c7d332521e8d5d9389455f98ebe947
Jonas Herget <jonas.herget@doubleslash.de>
2023-10-29 04:59:41 +0100

    Jonas Commit

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,17 +1,11 @@
 export interface User {
-    name:        string;
-    password:    string;
-    description: string;
-    birthday:    string;
-    experience:  string;
-    telephone:   string;
-    email:       string;
-    ratings:     Rating[];
-    id:          number;
-}
-
-export interface Rating {
-    rating:      number;
-    description: string;
-    id:          number;
+    name:           string;
+    password:       string;
+    description:    string;
+    birthday:       string;
+    experience:     string;
+    telephone:      string;
+    email:          string;
+    averageRating:  number;
+    id:             number;
 }
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,58 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonIcon, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import { Order } from '../interfaces/order';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../components/AuthContext';
+import axios from 'axios';
+import { star } from 'ionicons/icons';
 
 const Tab3: React.FC = () => {
+  const [orders, setOrders] = useState<Order[] | undefined>();
+
+  const { userId, } = useAuth();
+
+  const getOrders = async (user: string) => {
+    try {
+      setOrders((await axios.get<Order[]>(`https://erms.stefhol.eu/api/v1/user/${user}/order`, {})).data);
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  };
+  useEffect(() => {
+    getOrders(userId!);
+    console.table(orders)
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle style={{ textAlign: 'center' }}>ShareWheels</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+        <IonHeader style={{ padding: 10 }}>
+          <IonToolbar >
+            <IonTitle style={{ padding: 10, fontSize: 24 }} color="primary" class="ion-text-primary">Deine Buchungen</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        {orders?.map((order) => (
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle>{order.offer.car.model}</IonCardTitle>
+              <IonCardSubtitle>
+                von {order.offer.user.name} {!isNaN(order.offer.user.averageRating) ? order.offer.user.averageRating : "-"}/5.0
+                <IonIcon icon={star} style={{ paddingLeft:"0.3rem", color: "orange" }}></IonIcon>
+              </IonCardSubtitle>
+              <IonCardSubtitle>von {new Date(order.startDate).toDateString()} bis {new Date(order.endDate).toDateString()}</IonCardSubtitle>
+              <IonCardSubtitle>{order.priceInEuro}€ Mietkosten</IonCardSubtitle>
+            </IonCardHeader>
+            <img alt="image link" src={order.offer.car.imageUrl} />
+          </IonCard>
+        ))}
+
       </IonContent>
     </IonPage>
   );
